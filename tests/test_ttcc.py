b69b536ae1dbe34bb8e32c7b89d29e3605587571
yuanfei <yuanfei.lin@tum.de>
2021-10-14 11:22:24 +0200

    fix: the TTCC for second scenario should be 3.0

--- tests/test_ttcc.py
+++ tests/test_ttcc.py
@@ -1,16 +1,14 @@
 import os
 import unittest
 import math
-import matplotlib.pyplot as plt
-
-
-from commonroad.visualization.mp_renderer import MPRenderer
+from copy import deepcopy
 from commonroad.common.file_reader import CommonRoadFileReader
 from crmonitor.common.world_state import WorldState
 
 from cut_off.ttcc import TTCC
 from cut_off.simulation import SimulationLong, SimulationLateral, CutOffAction
-from cut_off.utils import check_velocity_feasibility, transfer_state_list_to_trajectory
+from cut_off.utils import check_velocity_feasibility, visualize_state_list
+
 
 class TestTTCC(unittest.TestCase):
     def setUp(self) -> None:
@@ -22,53 +20,76 @@ class TestTTCC(unittest.TestCase):
 
     def test_ttv(self):
         ego_id = 1003
+        scenario_copied = deepcopy(self.scenario)
         world_state_1 = WorldState.create_from_scenario(self.scenario,
                                                         ego_id)
-        ttcc_object_1 = TTCC(world_state_1, ["R_G1"])
-        ttv = ttcc_object_1.calc_ttv()
-        self.assertEqual(ttv, 2.0)
+        ego_vehicle_1 = self.scenario.obstacle_by_id(ego_id)
+        ttcc_object_1 = TTCC(world_state_1, self.scenario, ego_vehicle_1, ["R_G1"])
+        self.assertEqual(ttcc_object_1.ttv, 2.0)
         ego_id = 1002
-        world_state_2 = WorldState.create_from_scenario(self.scenario,
+        scenario_copied = self.scenario
+        world_state_2 = WorldState.create_from_scenario(scenario_copied,
                                                         ego_id)
-        ttcc_object_2 = TTCC(world_state_2, ["R_G1"])
-        ttv = ttcc_object_2.calc_ttv()
-        self.assertEqual(ttv, -math.inf)
+        ego_vehicle_2 = scenario_copied.obstacle_by_id(ego_id)
+        ttcc_object_2 = TTCC(world_state_2, scenario_copied, ego_vehicle_2, ["R_G1"])
+        self.assertEqual(ttcc_object_2.ttv, 3.0)
 
     def test_simulation_long(self):
         ego_id = 1003
         ego_vehicle = self.scenario.obstacle_by_id(ego_id)
         SL1 = SimulationLong(CutOffAction.KICKDOWN, ego_vehicle, 0)
         simulated_state1 = SL1.simulate_state_list()
-        self.assertEqual(simulated_state1[-1].time_step, 50)
-        self.assertEqual(check_velocity_feasibility(simulated_state1[-1], SL1.parameters), True)
+        self.assertEqual(
+            simulated_state1[-1].time_step,
+            50)
+        self.assertEqual(check_velocity_feasibility(
+            simulated_state1[-1],
+            SL1.parameters),
+            True)
         SL2 = SimulationLong(CutOffAction.STEADYSPEED, ego_vehicle, 0)
         simulated_state2 = SL2.simulate_state_list()
-        self.assertEqual(simulated_state2[-1].time_step, 50)
-        self.assertEqual(simulated_state2[-1].velocity, simulated_state2[0].velocity)
+        self.assertEqual(
+            simulated_state2[-1].time_step,
+            50)
+        self.assertEqual(
+            simulated_state2[-1].velocity,
+            simulated_state2[0].velocity)
         SL3 = SimulationLong(CutOffAction.KICKDOWN, ego_vehicle, 0)
         simulated_state3 = SL3.simulate_state_list()
-        self.assertEqual(simulated_state3[-1].time_step, 50)
-        self.assertEqual(check_velocity_feasibility(simulated_state3[-1], SL3.parameters), True)
+        self.assertEqual(
+            simulated_state3[-1].time_step,
+            50)
+        self.assertEqual(
+            check_velocity_feasibility(
+                simulated_state3[-1],
+                SL3.parameters),
+            True)
+        # visualize the scenario and the trajectory
+        # visualize_state_list(simulated_state3, self.scenario, SL3.parameters)
 
     def test_simulate_lateral(self):
         ego_id = 1003
         ego_vehicle = self.scenario.obstacle_by_id(ego_id)
         world_state: WorldState = WorldState.create_from_scenario(self.scenario, ego_id)
-        SL1 = SimulationLateral(CutOffAction.LANECHANGELEFT, ego_vehicle, 0, world_state)
+        SL1 = SimulationLateral(
+            CutOffAction.LANECHANGELEFT,
+            ego_vehicle,
+            0,
+            world_state)
         simulated_state_list1 = SL1.simulate_state_list()
-        final_lanelet = self.scenario.lanelet_network.find_lanelet_by_position([simulated_state_list1[-1].position])[0]
+        final_lanelet = self.scenario.lanelet_network.find_lanelet_by_position(
+            [simulated_state_list1[-1].position])[0]
         final_lane = world_state.road_network.find_lane_by_lanelet(final_lanelet[0])
-        self.assertEqual(world_state.ego_vehicle.lane.adj_left.lane_id, final_lane.lane_id)
+        self.assertEqual(
+            world_state.ego_vehicle.lane.adj_left.lane_id,
+            final_lane.lane_id)
         SL2 = SimulationLateral(CutOffAction.LANECHANGERIGHT, ego_vehicle, 0, world_state)
         simulated_state_list2 = SL2.simulate_state_list()
-        final_lanelet = self.scenario.lanelet_network.find_lanelet_by_position([simulated_state_list2[-1].position])[0]
+        final_lanelet = self.scenario.lanelet_network.find_lanelet_by_position(
+            [simulated_state_list2[-1].position])[0]
         final_lane = world_state.road_network.find_lane_by_lanelet(final_lanelet[0])
-        self.assertEqual(world_state.ego_vehicle.lane.adj_right.lane_id, final_lane.lane_id)
-        # rnd = MPRenderer()
-        # self.scenario.draw(rnd)
-        # trajectory1 = transfer_state_list_to_trajectory(self.scenario, simulated_state_list1,
-        #                                                 SL1.parameters)
-        # trajectory1.draw(rnd, draw_params={'time_begin': 0,
-        #                                    'trajectory': {'draw_trajectory': True}})
-        # rnd.render()
-        # plt.show()
+        self.assertEqual(
+            world_state.ego_vehicle.lane.adj_right.lane_id,
+            final_lane.lane_id)
+        # visualize the scenario and the trajectory
+        # visualize_state_list(simulated_state_list2, self.scenario, SL2.parameters)
