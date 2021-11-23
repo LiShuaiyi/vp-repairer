a4f52761fbe33b354164556567dbe5ef58905213
yuanfei <yuanfei.lin@tum.de>
2021-11-23 10:17:18 +0100

    bugfix: TTCC test - not to pandas

--- crrepairer/lazy_smt/monitor.py
+++ crrepairer/lazy_smt/monitor.py
@@ -13,15 +13,10 @@ class RuleMonitor:
     def __init__(self,
                  world_state,
                  rules: Union[str, Iterable[str]],
-                 monitor_type: MonitorType = MonitorType.STL,
-                 to_pandas: bool = True):
+                 monitor_type: MonitorType = MonitorType.STL,):
         self._rule_eval = RuleSetEvaluator.create_from_config(rules)
-        self._to_pandas = to_pandas
         self.world_state = world_state
         self.rob_rule, self.rob_predicate, self.rob_abstraction = self.evaluate_initially()
-        if self._to_pandas:
-            self.rob_rule, self.rob_predicate = self._rule_eval.result2pandas(self.rob_rule, self.rob_predicate)
-            self.rob_abstraction = self._rule_eval.abstraction2pandas(self.rob_abstraction)
 
     @property
     def rule_eval(self):
@@ -56,4 +51,5 @@ class RuleMonitor:
     def query_rule_rob_all(self):
         if self.rob_rule is None:
             raise ValueError("the evaluation procedure is not executed yet")
-        return self.rob_rule['robustness'].values
+        rob_rule, _ = self._rule_eval.result2pandas(self.rob_rule, self.rob_predicate)
+        return rob_rule['robustness'].values
