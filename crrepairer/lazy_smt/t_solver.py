a51346f7514d8deef26aa628590d1fe75ee88fdb
yuanfei <yuanfei.lin@tum.de>
2021-11-26 13:30:42 +0100

    add basic py files for Tsolver

--- crrepairer/lazy_smt/t_solver.py
+++ crrepairer/lazy_smt/t_solver.py
@@ -1,9 +1,18 @@
 from cut_off.tc import TC
+from cut_off.simulation import CutOffAction
 
 
 class TSolver:
     def __init__(self,
-                 sel_subform,
                  rule_monitor):
-        self._sel_subform = sel_subform
+        self._sel_subform = None
         self._tc_obj = TC(rule_monitor)
+        self._compliant_maneuvers = list()
+        self._tc_dict = dict()
+
+    def assign_subformula(self, subformula):
+        self._sel_subform = subformula
+
+    def set_compliant_maneuver(self):
+        assert self._sel_subform is not None, "<T-Solver>: the subformula needs to be assigned first for the T-solver"
+        pass
