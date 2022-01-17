1ba1dedc64e0fc168a3262b2e2b68cc828b35784
yuanfei <yuanfei.lin@tum.de>
2022-01-17 17:54:09 +0100

    add RG2

---
+++ tests/sc1_RG2.py
@@ -0,0 +1,45 @@
+from abstraction.abstracter import RuleAbstracter
+from t_solver.t_solver import TSolver
+from t_solver.qp_planner import QPPlannerRepair
+from repairer.smt_repairer import SMTTrajectoryRepairer
+from t_solver.utils import convert_traj_to_ego_vehicle
+from commonroad_repair.crrepairer.repairer.visualization import visualize_repairing_result, visualize_v_profile
+
+from commonroad.common.file_reader import CommonRoadFileReader
+from commonroad.visualization.mp_renderer import MPRenderer
+from commonroad.visualization.param_server import ParamServer
+import matplotlib.pyplot as plt
+import math
+
+scenario_id = "DEU_LocationAUpper-26_21_T-1"
+
+file_path = "/home/yuanfei/commonroad/highD-dataset/highD-cr-scenarios/" \
+            + scenario_id + ".xml"
+
+if __name__ == '__main__':
+    scenario, planning_problem_set = CommonRoadFileReader(file_path).open(lanelet_assignment=True)
+    # self.scenario.remove_obstacle(self.scenario.obstacle_by_id(1006))
+    planning_problem = list(planning_problem_set.planning_problem_dict.values())[0]
+    ego_id = 49
+    rule = "R_G2"
+
+    time_step = 0
+    rnd = MPRenderer(figsize=(40, 10))
+    scenario.draw(
+        rnd,
+        draw_params=ParamServer({"time_begin": time_step, "trajectory": {
+                 "draw_trajectory": False}, "occupancy": {
+            "draw_occupancies": 0}, 'dynamic_obstacle': {'show_label': True}})
+    )
+    rnd.render()
+    plt.title(str(time_step))
+    plt.show()
+    ego_initial = scenario.obstacle_by_id(ego_id)
+
+    rule_abstracter = RuleAbstracter(scenario,
+                                     planning_problem,
+                                     ego_id, rule)
+
+    repairer = SMTTrajectoryRepairer(rule_abstracter,
+                                     ego_initial)
+    repaired_traj = repairer.repair()
