a1fb9d473237354d6ae0bb0a8246eb6dbfb78481
yuanfei <yuanfei.lin@tum.de>
2022-01-27 08:12:57 +0100

    rename figures (RG1) add video

---
+++ tests/figures/convert_png2mp4.py
@@ -0,0 +1,14 @@
+# 1-2
+rule = "R_G1"
+# mode = 'initial'
+mode = 'repaired'
+
+import ffmpeg
+(
+    ffmpeg
+    .input('/home/yuanfei/commonroad/commonroad_repair/tests/figures/' +
+           '/rg' + rule[-1] + '_' + mode +
+           '/*.svg', pattern_type='glob', framerate=10)
+    .output(rule + mode + '.mp4')
+    .run()
+)
