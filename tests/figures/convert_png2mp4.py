4cd0c845103091a780e520221caeaf2bac580b90
yuanfei <yuanfei.lin@tum.de>
2022-02-11 21:10:51 +0100

    update video's frame rate

--- tests/figures/convert_png2mp4.py
+++ tests/figures/convert_png2mp4.py
@@ -8,7 +8,7 @@ import ffmpeg
     ffmpeg
     .input('/home/yuanfei/commonroad/commonroad_repair/tests/figures/' +
            '/rg' + rule[-1] + '_' + mode +
-           '/*.svg', pattern_type='glob', framerate=10)
+           '/*.svg', pattern_type='glob', framerate=20)
     .output(rule + mode + '.mp4')
     .run()
 )
