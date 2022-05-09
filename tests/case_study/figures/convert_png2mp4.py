a51bd08e8995ba318b017b0cdb8087fbafdb49cb
yuanfei <yuanfei.lin@tum.de>
2022-05-09 13:45:16 +0200

    update videos

--- tests/case_study/figures/convert_png2mp4.py
+++ tests/case_study/figures/convert_png2mp4.py
@@ -1,14 +1,14 @@
-# 1-2
-rule = "R_G3"
+# 1-3
+rule = "R_G1"
 # mode = 'initial'
 mode = 'repaired'
 
 import ffmpeg
 (
     ffmpeg
-    .input('/home/yuanfei/commonroad/commonroad_repair/tests/figures/' +
+    .input('/home/yuanfei/commonroad/commonroad_repair/tests/case_study/figures/' +
            '/rg' + rule[-1] + '_' + mode +
-           '/*.svg', pattern_type='glob', framerate=20)
+           '/*.svg', pattern_type='glob', framerate=10)
     .output(rule + mode + '.mp4')
     .run()
 )
