95a7b5192fb3ad10fab4d278dee2ed3fa12e999c
Yuanfei Lin <yuanfei.lin@tum.de>
2022-12-22 16:23:02 +0000

    Bump version: 0.0.0.dev2 → 0.0.0.dev3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="commonroad_repairer",
-    version="0.0.0.dev2",
+    version="0.0.0.dev3",
     author="Cyber-Physical Systems Group, Technical University of Munich",
     author_email="commonroad@lists.lrz.de",
     description="It's pip... with git.",
