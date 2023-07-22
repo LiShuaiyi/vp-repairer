e962dee4bfbe8220cb91d57fb8d044897818f42d
Zekun Xing <zekun.xing@tum.de>
2023-07-22 17:51:12 +0200

    add smt for miqp

---
+++ crrepairer/repairer/smt_repairer_miqp.py
@@ -0,0 +1,107 @@
+from abc import ABC
+import math
+import time
+
+from commonroad.scenario.scenario import DynamicObstacle
+from commonroad.scenario.trajectory import Trajectory
+from commonroad.scenario.obstacle import TrajectoryPrediction, ObstacleType
+from commonroad.planning.planning_problem import PlanningProblem
+
+from crrepairer.repairer.base import TrajectoryRepair
+from crrepairer.smt.monitor_wrapper import STLRuleMonitor
+from crrepairer.smt.sat_solver.sat_solver import SATSolver
+from crrepairer.smt.t_solver.t_solver_miqp import TSolver
+
+from z3 import sat
+from enum import Enum
+
+
+class RepairingRule(Enum):
+    COLLISION_FREE = "collision free"
+    R_G1 = "R_G1"
+    R_G2 = "R_G2"
+    R_G3 = "R_G3"
+
+
+class SMTTrajectoryRepairer(TrajectoryRepair, ABC):
+    def __init__(self,
+                 rule_monitor: STLRuleMonitor,
+                 planning_problem: PlanningProblem,
+                 ego_vehicle: DynamicObstacle):
+        super().__init__(ego_vehicle.prediction.trajectory)
+        self.rule_monitor = rule_monitor
+        self._inital_rob = (self.rule_monitor.rob_rule, 
+                            rule_monitor.rob_predicate, 
+                            rule_monitor.rob_abstraction)
+        self._model = None
+        self._tc = -math.inf
+        self._tv = -math.inf
+        # initialize Solvers for SMT paradigm
+        self.sat_solver = SATSolver(self.rule_monitor)
+        self.t_solver = TSolver(ego_vehicle, planning_problem, self.rule_monitor)
+
+    @property
+    def tv(self):
+        return self._tv
+
+    @property
+    def tc(self):
+        return self._tc
+
+    @property
+    def model(self):
+        """
+        SAT models
+        """
+        return self._model
+
+    def repair(self, check_flag=True, *args, **kwargs):
+        self._tv = self.rule_monitor.tv_time_step
+        if self._tv == -math.inf:
+            return None
+        nr = 1
+        print("******** Trajectory Repairing starts! ********")
+        start_time = time.time()
+        while self.sat_solver.solve() == sat:
+            print("* {}. iteration...".format(nr))
+            if self.rule_monitor.proposition_nodes is None:
+                return None
+            select_proposition, self._model = self.sat_solver.model()
+            repairability, repaired_traj = self.t_solver.check(select_proposition, list(self._model))
+            self._tc = self.t_solver.tc_object.tc_time_step
+            if repairability and repaired_traj is not None:
+                print(f"----- Computation Time: {time.time() - start_time:.3f}s -----")
+                tv, _ = self.t_solver.tc_object.calc_tv_updated(repaired_traj.state_list, int(self._tc))
+                if tv == math.inf or not check_flag:
+                    print("*****  Successfully Repaired! •ᴗ•  *****")
+                    return repaired_traj
+                else:
+                    print("*** Reparable but Solver Failed ಠ_ಠ  ***")
+            self.sat_solver.update_formula()
+            nr += 1
+        print("*******   Repairing Failed ಠ_ಠ   *******")
+        return None
+
+    @staticmethod
+    def convert_traj_to_ego_vehicle(shape,
+                                    initial_state,
+                                    cr_trajectory: Trajectory,
+                                    vehicle_id: int = 0) -> DynamicObstacle:
+        """
+        Converts trajectory object to CommonRoad obstacle with specified width and length
+        :param width: The width of the ego vehicle
+        :param length: The length of the ego vehicle
+        :param vehicle_id: ID of ego vehicle
+        :return: The CommonRoad DynamicObstacle object containing the current trajectory
+        """
+        # get trajectory
+        pred = TrajectoryPrediction(cr_trajectory, shape)
+
+        # create new object
+        ego = DynamicObstacle(obstacle_id=vehicle_id,
+                              obstacle_type=ObstacleType.CAR,
+                              prediction=pred,
+                              obstacle_shape=shape,
+                              initial_state=initial_state)
+        return ego
+
