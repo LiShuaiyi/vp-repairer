b9c9d733d37ed62063ed22c7cfc9d4e8964bd570
Zekun Xing <zekun.xing@tum.de>
2023-10-15 09:21:03 +0200

    add future time

--- crrepairer/cut_off/tc.py
+++ crrepairer/cut_off/tc.py
@@ -42,7 +42,7 @@ class TC(CutOffBase, ABC):
             self.round_tolerance = 1
         self.rule_monitor = rule_monitor_copy
         self._world_ego = self.world.vehicle_by_id(ego_vehicle.obstacle_id)
-        self._tv_time_step = self.rule_monitor.tv_time_step
+        self._tv_time_step = self.rule_monitor.tv_time_step + self.rule_monitor.furture_time_step
         self._other_id = self.rule_monitor.other_id
         self._visualize = False
         self._compliant_maneuver = None
