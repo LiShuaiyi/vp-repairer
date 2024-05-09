94404a7481464977dcd7dd3f53578cbff2b79039
Yuanfei <yuanfei.lin@tum.de>
2024-05-09 09:29:46 +0200

    fix clcs

--- crrepairer/smt/t_solver/miqp_planner_repair.py
+++ crrepairer/smt/t_solver/miqp_planner_repair.py
@@ -288,7 +288,7 @@ class MIQPPlannerRepair(MIQPPlanner):
         """
         cartesian_traj_points = list()
         for state in trajectory.states:
-            cart_pos = self.vehicle_configuration.qp_veh_config.curvilinear_coordinate_system.convert_to_cartesian_coords(
+            cart_pos = self.vehicle_configuration.qp_veh_config.CLCS.convert_to_cartesian_coords(
                 state.position[0], state.position[1]
             )
             orientation_interpolated = np.interp(
