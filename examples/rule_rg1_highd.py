55f91f29670e458030610f6a8a40db375f8d59b3
Yuanfei <yuanfei.lin@tum.de>
2024-08-27 18:17:58 +0200

    nicer highDscenario

--- examples/rule_rg1_highd.py
+++ examples/rule_rg1_highd.py
@@ -10,14 +10,14 @@ import math
 if __name__ == "__main__":
     # ========== Scenario and Configuration =========
     scenario_id = "DEU_LocationAUpper-54_67_T-1" # id=9
-    scenario_id = "DEU_LocationFUpper-60_211_T-1" # id=15
-    scenario_id = "DEU_LocationALower-26_189_T-1" # id=19
+    scenario_id = "DEU_LocationELower-18_22_T-1" # id=11
+    # scenario_id = "DEU_LocationALower-26_189_T-1" # id=19
     # Build configuration object
     config = RepairerConfiguration()
     config.general.set_path_scenario(scenario_id)
     config.update()
     config.repair.rules = ["R_G1"]
-    config.repair.ego_id = 19
+    config.repair.ego_id = 14
     config.debug.show_plots = True
     config.repair.planner = 2
     config.repair.constraint_mode = 2
