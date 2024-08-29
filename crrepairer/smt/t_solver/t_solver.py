a75406695bf420cf50d6c5877a55ec1b0125b2b7
Yuanfei <yuanfei.lin@tum.de>
2024-08-29 11:21:18 +0200

    minor fix, add verbos

--- crrepairer/smt/t_solver/t_solver.py
+++ crrepairer/smt/t_solver/t_solver.py
@@ -84,6 +84,11 @@ class TSolver:
             #     prop.ttv_value > 0 and "~" + prop.alphabet in model
             # ):
             #     self._sel_prop.append(prop)
+            # print out the selected propositions
+            if self.verbose:
+                print(
+                    f"* \t<TSolver>: selected propositions: {prop.alphabet} = {prop.ttv_value}"
+                )
         self._compliant_maneuvers = self.set_compliant_maneuver(use_mpr_derivative)
 
     def set_compliant_maneuver(self, use_mpr_derivative: bool):
