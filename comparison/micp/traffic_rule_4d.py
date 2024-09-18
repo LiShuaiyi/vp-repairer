ec5359491ffc0e23e5fc7f91e743d8286d840096
Yuanfei <yuanfei.lin@tum.de>
2024-09-18 10:32:56 +0200

    add formulation of the rule RIN4

--- comparison/micp/traffic_rule_4d.py
+++ comparison/micp/traffic_rule_4d.py
@@ -1,7 +1,12 @@
+import shapely
+
+from typing import List
+
+from matplotlib import pyplot as plt
 from stlpy.benchmarks.base import BenchmarkScenario
 from stlpy.STL.formula import STLTree
 from stlpy.systems.linear import DoubleIntegrator
-from stlpy.benchmarks.common import make_rectangle_patch
+from stlpy.benchmarks.common import make_rectangle_patch, inside_rectangle_formula, outside_rectangle_formula
 
 from comparison.micp.formula import (in_front_of_formula, in_same_lane_formula, keeps_safe_distance_formula,
                                      collision_free_formula,
@@ -17,7 +22,7 @@ from crmonitor.common.world import World
 
 from commonroad.scenario.traffic_sign import SupportedTrafficSignCountry
 from commonroad.scenario.traffic_sign_interpreter import TrafficSignInterpreter
-
+from commonroad.scenario.lanelet import LaneletType, Lanelet
 
 class RG1(BenchmarkScenario):
     """Rule RG1"""
@@ -336,3 +341,112 @@ class RG123(BenchmarkScenario):
             ax.add_patch(obstacle)
         ax.set_ylim((0, 10))
         ax.set_aspect('equal')
+
+class RIN4(BenchmarkScenario):
+    """Rule RIN4"""
+    def __init__(self, T: int, world: World, ego_vehicle: Vehicle, other_vehicle: Vehicle, lanelet_network):
+        self.T: int = T
+        self.ego_vehicle = ego_vehicle
+        self.other_vehicle = other_vehicle
+        self.lanelet_network = lanelet_network
+
+        self.conflict_area = self.obtain_conflict_area()
+        self.conflict_area_bound = self.conflict_area.bounds # (xmin, xmax, ymin, ymax)
+        print(f"Conflict Area Bounds: {self.conflict_area_bound}")
+
+    def obtain_conflict_area(self):
+        lanelets_dir_ego = self.ego_vehicle.lanelets_dir
+        lanelets_dir_other = self.other_vehicle.lanelets_dir
+
+        # Calculate the union of the ego lanelets and other vehicle's lanelets at intersections
+        lanelets_ego_intersection = self.get_intersection_lanelets(self.lanelet_network, lanelets_dir_ego)
+        lanelets_other_intersection = self.get_intersection_lanelets(self.lanelet_network, lanelets_dir_other)
+
+        ego_intersection_region = self.get_lanelet_union(lanelets_ego_intersection)
+        other_intersection_region = self.get_lanelet_union(lanelets_other_intersection)
+
+        conflict_region_CART = ego_intersection_region.intersection(other_intersection_region)
+
+        vehicle_length_add = self.ego_vehicle.shape.length / 2
+
+        conflict_region_enlarged = shapely.offset_curve(
+            conflict_region_CART, vehicle_length_add
+        )
+
+        conflict_region_enl_polygon = shapely.Polygon(conflict_region_enlarged)
+
+        conflict_region_enl_clcs = [self.ego_vehicle.ref_path_lane.CLCS.convert_to_curvilinear_coords(
+            vertex[0], vertex[1]) for vertex in conflict_region_enl_polygon.exterior.coords]
+
+        conflict_region_enl_clcs_polygon = shapely.Polygon(conflict_region_enl_clcs)
+
+        # Plotting both the Cartesian and Curvilinear conflict regions
+        fig, ax = plt.subplots(1, 2, figsize=(12, 6))
+
+        # Plot Cartesian conflict region
+        ax[0].set_title("Conflict Region in Cartesian Coordinates")
+        ax[0].plot(*conflict_region_CART.exterior.xy, color='blue', label='Original Region')
+        ax[0].plot(*conflict_region_enl_polygon.exterior.xy, color='red', linestyle='--', label='Enlarged Region')
+        ax[0].set_aspect('equal', 'box')
+        ax[0].legend()
+
+        # Plot Curvilinear conflict region
+        ax[1].set_title("Conflict Region in Curvilinear Coordinates")
+        ax[1].plot(*zip(*conflict_region_enl_clcs), color='green', label='Enlarged CLCS Region')
+        ax[1].set_aspect('equal', 'box')
+        ax[1].legend()
+
+        # Show the plots
+        plt.show()
+
+        return conflict_region_enl_clcs_polygon
+
+
+    @staticmethod
+    def get_lanelet_union(lanelets: List[Lanelet]) -> shapely.geometry.base.BaseGeometry:
+        # Create a union of all lanelet geometries
+        return shapely.unary_union([lanelet.polygon.shapely_object for lanelet in lanelets])
+
+    @staticmethod
+    def get_intersection_lanelets(lanelet_network, lanelet_ids):
+        return [
+            lanelet_network.find_lanelet_by_id(lanelet_id)
+            for lanelet_id in lanelet_ids
+            if LaneletType.INTERSECTION in lanelet_network.find_lanelet_by_id(lanelet_id).lanelet_type
+        ]
+
+    def GetSpecification(self):
+        # inside the conflict area
+        inside_conflict_area = inside_rectangle_formula(self.conflict_area_bound, 0, 1, 10)
+
+        # outside the conflict area
+        outside_conflict_area = outside_rectangle_formula(self.conflict_area_bound, 0, 1, 10)
+
+        # Put all the constraints together in one specification
+        # vehicle 10180 stays in the conflict area from time step 9 to 13
+        #     (
+        #         (
+        #             (in_intersection_conflict_area(a0, a1)) implies
+        #         (
+        #         not (causes_braking_intersection(a0, a1))
+        #     and not (eventually[0, 1s](in_intersection_conflict_area(a1, a0)))
+        #     )
+        #     ) and
+        #     (
+        #         (in_intersection_conflict_area(a1, a0)) implies
+        #     not (eventually[0, 0.6s](in_intersection_conflict_area(a0, a1)))
+        #     )
+        #     ) or not (on_lanelet_with_type_intersection(a0))
+        # )
+        other_veh_in_conflict_area_time = [9, 13]
+        spec = (outside_conflict_area.always(other_veh_in_conflict_area_time[0], other_veh_in_conflict_area_time[1] + 3) &
+                inside_conflict_area.eventually(other_veh_in_conflict_area_time[-1], self.T))
+
+        return spec
+
+    def GetSystem(self):
+        sys = VehicleModel(0.2)
+        return sys
+
+    def add_to_plot(self, ax):
+        pass
