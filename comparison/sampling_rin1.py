0505ae4ccf9b85ff4ff783010b1b83ab876f5c4b
Yuanfei <yuanfei.lin@tum.de>
2024-10-17 12:21:47 +0200

    adjust the rin1 sampling params

--- comparison/sampling_rin1.py
+++ comparison/sampling_rin1.py
@@ -185,8 +185,8 @@ def main(
         if plan_new_trajectory:
             # new planning cycle -> plan a new optimal trajectory
             planner.set_desired_velocity(current_speed=planner.x_0.velocity,
-                                         desired_velocity=0)
-            # planner.set_v_sampling_parameters(0.01, 2)
+                                         desired_velocity=0,)
+            planner.set_v_sampling_parameters(0.01, 15)
 
             if SAMPLING_ITERATION_IN_PLANNER:
                 optimal = planner.plan()
